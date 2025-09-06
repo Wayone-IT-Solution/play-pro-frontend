ba16537de177076d5de00db021c97a3a43e384ba
rishabh-wayone <rishabh@wayone.co.in>
2025-09-06 11:42:32 +0530

    fix

--- src/components/home/PlayProBanner.tsx
+++ src/components/home/PlayProBanner.tsx
@@ -8,7 +8,7 @@ const PlayProBanner = ({ banners }: { banners?: any }) => {
       {/* Left Player Image */}
       <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[300px] sm:w-[380px] lg:w-[460px] h-auto">
         <Image
-          src="/assets/player1.png"
+          src="/assets/profile1.png"
           alt="Football Player Left"
           width={460}
           height={460}
@@ -19,7 +19,7 @@ const PlayProBanner = ({ banners }: { banners?: any }) => {
       {/* Right Player Image */}
       <div className="absolute right-0 top-1/2 -translate-y-1/2 w-[300px] sm:w-[380px] lg:w-[460px] h-auto">
         <Image
-          src="/assets/player2.png"
+          src="/assets/profile22.png"
           alt="Football Player Right"
           width={460}
           height={460}
@@ -28,9 +28,9 @@ const PlayProBanner = ({ banners }: { banners?: any }) => {
       </div>
 
       {/* Center Content */}
-      <div className="relative z-10 max-w-5xl mx-auto text-center px-4 py-16 sm:py-24">
+      <div className="relative z-10 max-w-5xl mx-auto text-center px-4 py-16 sm:pb-24">
         {/* Title */}
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold leading-snug mb-28 text-[#2F0D46]">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold leading-snug mb-52 text-[#2F0D46]">
           PlayPro simplifies <span className="text-[#2F0D46]">sp</span>
           <span className="text-gray-300">ort</span>
           <span className="text-[#2F0D46]">s</span>
